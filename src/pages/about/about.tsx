871f5fdebbcc141d77d152fb6599c39bc3da64f6
Liangzhihe <1789211411@qq.com>
2023-06-16 17:03:31 +0800

    feat: [pages] init about page

---
+++ src/pages/about/about.tsx
@@ -0,0 +1,13 @@
+// about me
+const About = () => {
+  return (
+    <div>
+      <h1>About me</h1>
+      // TODO: add github link
+      // add other links, like email, website etc.
+      <p>My name is Liangzhihe and I'm a web developer</p>
+    </div>
+  )
+}
+
+export default About
